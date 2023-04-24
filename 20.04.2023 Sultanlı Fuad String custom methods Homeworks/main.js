039c5509ef8eb132c5c6dedd214a06a3ac4a06b3
AZEFUAD <fuadkhs@code.edu.az>
2023-04-24 16:35:23 +0400

    Successfully committed.

---
+++ 20.04.2023 Sultanlı Fuad String custom methods Homeworks/main.js
@@ -0,0 +1,38 @@
+//1-ci məsələnin cavabı:
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };
+
+  let sentence= 'hello Emir Timur';
+  console.log(sentence.capitalize());
+
+
+  //2-ci məsələnin cavabı:
+//   String.prototype.reverse = function() {
+//     let reversedString = '';
+//     for(let i = this.length - 1; i >= 0; i--) {
+//       reversedString += this.charAt(i);
+//     }
+//     return reversedString;
+//   };
+
+//   let sentence= 'good morning math';
+//   console.log(sentence.reverse());
+
+  //3-cü məsələnin cavabı:
+//   String.prototype.capitalizeVowels = function() {
+//     let vowels = ['a', 'e', 'i', 'o', 'u'];
+//     let newString = '';
+//     for(let i = 0; i < this.length; i++) {
+//       let char = this.charAt(i);
+//       if(vowels.includes(char.toLowerCase())) {
+//         newString += char.toUpperCase();
+//       } else {
+//         newString += char;
+//       }
+//     }
+//     return newString;
+//   };
+
+//   let sentence= 'hello javascript';
+//   console.log(sentence.capitalizeVowels());
